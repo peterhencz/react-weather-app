f87d8c9cf26d753e59ea8e9976792fbcc6481497
Peter Hencz <peter.hencz@gmail.com>
2018-09-26 16:16:29 +0200

    create form and weather components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,50 @@ import React from "react";
 
 import Titles from "./components/titles";
 import Form from "./components/form";
+import Weather from "./components/weather";
+
+const API_KEY = "7257dfb135c8fcd4ac3b2fbf49fadc31";
 
 class App extends React.Component {
+
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+  
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    const data = await api_call.json();
+    console.log(data);
+    this.setState({
+      temperature: Math.floor(data.main.temp - 273),
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    });
+  }
   render() {
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather}/>
+        <Weather 
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }
--- src/components/weather.js
+++ src/components/weather.js
@@ -2,10 +2,16 @@ import React from "react";
 
 class Weather extends React.Component {
   render() {
+    console.log()
     return (
-      <div>Weather</div>
+      <div>
+        { this.props.city && this.props.country && <p>Location: { this.props.city }, { this.props.country }</p> }
+        { this.props.temperature && <p>Temperature: { this.props.temperature }</p> }
+        { this.props.humidity && <p>Humidity: { this.props.humidity }</p> }
+        { this.props.description && <p>Description: { this.props.description }</p> }
+      </div>
     );
   }
 };
 
-export default Weather
+export default Weather;
