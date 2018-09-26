8883046e4ef671496053c478702e2a0bcd9afadb
Peter Hencz <peter.hencz@gmail.com>
2018-09-26 14:44:29 +0200

    create react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Titles from "./components/titles";
+import Form from "./components/form";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Titles />
+        <Form />
+      </div>
+    );
+  }
+};
+
+export default App;
