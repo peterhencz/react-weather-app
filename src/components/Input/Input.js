8ec69525695002eea4f03908f4b5784480167a84
Peter Hencz <35300697+peterhencz@users.noreply.github.com>
2018-10-10 19:30:35 +0200

    Update Input.js

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./input.scss";
+import "./Input.scss";
 
 const Input = ({ name, placeholder, value }) => (
   <input type="text" name={name} placeholder={placeholder} value={value} />
